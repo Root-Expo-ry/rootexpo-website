1d15002126a4055168b4b7a3e5d0d1939139c3fc
Onnimanni Hannonen <onnari11@gmail.com>
2025-03-22 17:35:06 +0200

    fix fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
-    fontFamily: {
-      Montserrat: ['Montserrat', 'sans-serif'],
-      JetBrainsMono: ['JetBrains Mono', 'monospace'],
-    }
   },
   plugins: [],
 };
