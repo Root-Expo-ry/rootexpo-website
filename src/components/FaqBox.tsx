40fb1324e16e6e096750d1cf48328a684fc97c5f
Oskari Arvo <oskari.e.arvo@utu.fi>
2025-07-23 15:17:03 +0300

    Updated the style of FAQ

--- src/components/FaqBox.tsx
+++ src/components/FaqBox.tsx
@@ -1,3 +1,4 @@
+import { span } from 'framer-motion/client';
 import { useState } from 'react';
 
 interface Props {
@@ -15,12 +16,14 @@ export default function FaqBox({ question, children }: Props) {
   return (
     <div
       onClick={() => setIsOpen(!isOpen)}
-      className="mb-4 cursor-pointer rounded-md border border-white bg-background p-4 shadow-sm 
+      className="mb-4 cursor-pointer rounded-md border border-background bg-container p-4 shadow-sm 
                  transition-all duration-50 active:scale-[0.98] hover:border-accent"
     >
       <div className="flex justify-between items-center text-white">
         <h3 className='font-semibold text-lg mt-2 -ml-0.5 pl-3'>{question}</h3>
-        <span>{isOpen ? '▲' : '▼'}</span>
+        <span className='text-4xl mr-3'>
+          {isOpen ? '−' : '+'}
+        </span>
       </div>
 
       <div
@@ -28,9 +31,8 @@ export default function FaqBox({ question, children }: Props) {
           isOpen ? 'max-h-[500px] mt-7 mb-5 opacity-100' : 'max-h-0 opacity-0'
         }`}
       >
-        <div className="flex items-start ml-3">
-          <span>▶︎</span>
-          <div className="text-white text-lg space-y-2">
+        <div className="flex items-start">
+          <div className="text-white text-lg space-y-2 font-montserrat">
             {children}
           </div>
         </div>
@@ -38,3 +40,5 @@ export default function FaqBox({ question, children }: Props) {
     </div>
   );
 }
+
+// ▲ ▼ — ▶︎
